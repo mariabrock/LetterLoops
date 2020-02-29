6fb9f155ab07fa8dfbef432ac9e8d5f82278d550
Maria <mariabrock414@gmail.com>
2020-02-29 12:36:21 -0600

    wip

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -6,10 +6,18 @@ namespace LetterLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter abcd.");
-            var userEntry1 = Console.ReadLine();
-            Console.WriteLine("A-Bb-Ccc-Dddd");
+            Console.WriteLine("Please type a string of letters.");
+            var userEntry = Console.ReadLine();
+            int count = 0;
+            foreach (char letter in userEntry)
+            {
+            count++;
+            }
+            Console.WriteLine(count);
+            count = 0;
 
+
+                Console.ReadKey();
         }
     }
 }

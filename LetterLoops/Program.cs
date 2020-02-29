8de74ab72ed707a2dc6256e015eec212b6d4c04f
Maria <mariabrock414@gmail.com>
2020-02-29 11:06:16 -0600

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter abcd.");
+            var userEntry1 = Console.ReadLine();
+            Console.WriteLine("A-Bb-Ccc-Dddd");
+
+        }
+    }
+}
